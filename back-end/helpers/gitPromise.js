280c1f87cacc50b2f5321e91f39cf9efe8f44a4f
edwinthomasg <edwinthomasg29@gmail.com>
2022-12-20 18:00:05 +0530

    files uploaded

--- back-end/helpers/gitPromise.js
+++ back-end/helpers/gitPromise.js
@@ -4,9 +4,7 @@ const gitPromise = (branch) => {
     return new Promise((resolve, reject) => {
         const worker = new Worker("./helpers/gitWorker.js")
         worker.postMessage(branch)
-        worker.on("message", (data) => {
-            console.log(data)
-        })
+        worker.on("message", resolve)
         worker.on("error", (err) => {
             console.log(err)
         })
