e0fec305937e2c4c59d7d773e274ee60c88f9f6c
edwinthomasg <edwinthomasg29@gmail.com>
2022-12-26 19:25:59 +0530

    change

--- back-end/helpers/dynamicData.js
+++ back-end/helpers/dynamicData.js
@@ -2,6 +2,7 @@ const { execFile } = require("child_process")
 const path = require("path")
 
 const generateData = () => {
+    console.log("generate data...")
     execFile(`${path.dirname(__dirname)}/executables/utilities.sh`, (err, stdout, stderr) => {
         if(err) {
             console.log("dynamic error : ",err)
